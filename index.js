ff4bad629a4fc8054b5242a8b6a7c1e345d23590
Nicholas Frangione <nfrangione8@gmail.com>
2021-01-28 22:56:53 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -20,8 +20,13 @@ function nowServing(katzDeliLine) {
 }
 
 function currentLine(katzDeliLine) {
-  var line = [];
-  for (let i = 0; i < katzDeliLine.length; i++) {
-    line.push()
+  if (katzDeliLine.length === 0) {
+    return "The line is currently empty."
+  }
+  else {
+    var line = [];
+    for (let i = 0; i < katzDeliLine.length; i++) {
+      line.push()
+    }
   }
 }
