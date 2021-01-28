06ddea91fc6ae985e6b969bea95c01fb15171082
Nicholas Frangione <nfrangione8@gmail.com>
2021-01-28 23:00:48 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ var katzDeliLine = [];
 
 function takeANumber(katzDeliLine, name) {
   katzDeliLine.push(`${name}`);
-  return `Welcome, ${name}. You are number ${katzDeliLine.length} in line.`
+  return `Welcome, ${name}. You are number ${katzDeliLine.length} in line.`;
 }
 
 function nowServing(katzDeliLine) {
@@ -12,7 +12,7 @@ function nowServing(katzDeliLine) {
   }
   if (katzDeliLine.length === 0)
   {
-    return "There is nobody waiting to be served!"
+    return "There is nobody waiting to be served!";
   }
   else {
     return `Currently serving ${katzDeliLine.shift()}.`;
@@ -26,7 +26,7 @@ function currentLine(katzDeliLine) {
   else {
     var line = [];
     for (let i = 0; i < katzDeliLine.length; i++) {
-      line.push(`${i+1}. ${line[i]}`)
+      line.push(`${i+1}. ${katzDeliLine[i]}`);
     }
     return `The line is currently: ` + line;
   }
